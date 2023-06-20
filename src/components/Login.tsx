3fbdeeb7995e10756a54632074536132530fced1
Rajath <rajath.kumar@novigo.com>
2023-06-20 19:51:27 +0530

    Jest and React-test-renderer added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+// import {  } from "@assets/";
 
 export const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function saveUserInfo(e: React.FormEvent) {
+    e.preventDefault();
+    setUserName("Rajat");
+    setPassword("Rajat123");
+  }
+
   return (
     <div>
-      <input type="text" name="username" id="username" />
-      <input type="password" name="password" id="password" />
-      <button type="submit">Login</button>
+      <form method="post" onSubmit={saveUserInfo}>
+        <input type="text" name="username" id="username" />
+        <input type="password" name="password" id="password" />
+        <button type="submit">Login</button>
+      </form>
+      <input type="text" value={userName} name="dUserName" id="dUserName" />
+      <input type="text" value={password} name="dPassword" id="dPassword" />
     </div>
   );
 };
--- test/components/Login.test.tsx
+++ test/components/Login.test.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+import { Login } from "../../src/components/Login";
+import renderer from "react-test-renderer";
+
 describe("Testing the Login component", () => {
-  it("enter username and password", () => {
+  test("test 1 equal to 1", () => {
     expect(1).toBe(1);
+  });
+  test("enter username and password", () => {
+    const component = renderer.create(<Login />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
